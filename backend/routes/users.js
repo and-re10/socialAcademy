3d36ae512bb02595196eb3724fddd6463ff8905b
and-re10 <andreandre_1234@hotmail.com>
2021-02-06 20:28:53 +0100

    backend users donne

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,23 +2,6 @@ const express = require('express');
 const Router = express();
 const User = require('../models/User');
 
-// Route pour ajouter un nouveau Utilisateur
-Router.post('/store', async (req, res) => {
-    const user = new User({
-        name: req.body.userName,
-        age: req.body.userAge,
-        role: req.body.userRole
-    });
-
-    try {
-        const saveUser = await user.save();
-        console.log(saveUser);
-        res.send(saveUser);
-    } catch (error) {
-        console.error(error);
-    }
-});
-
 // Route pour afficher les Utilisateur existants
 Router.get("/showAll", async (req, res) => {
     try {
@@ -26,13 +9,47 @@ Router.get("/showAll", async (req, res) => {
         res.send(users);
     } catch (error) {
         console.error(error)
-    }
+    };
 });
 
 // Route pour afficher un Utilisateur spécifique
 Router.get('/showOne/:userId', async (req, res) => {
     try {
         const user = await User.findOne({ _id: req.params.userId });
+        res.send(user);
+        console.log(user);
+    } catch (error) {
+        console.log(error);
+    };
+});
+
+// Route pour ajouter un nouveau Utilisateur
+Router.post('/store', async (req, res) => {
+    const user = new User({
+        name: req.body.userName,
+        age: req.body.userAge,
+        role: req.body.userRole
+    });
+    
+    try {
+        const saveUser = await user.save();
+        console.log(saveUser);
+        res.send(saveUser);
+    } catch (error) {
+        console.error(error);
+    };
+});
+
+// Route pour actualizer un Utilisateur existant
+Router.put('/update/:userId', async (req, res) => {
+    try {
+        const user = await User.updateOne({
+            _id: req.params.userId
+        },{
+            name: req.body.userName,
+            age: req.body.userAge,
+            role: req.body.userRole
+        }); // (filter, data updated)
         res.send(user);
         console.log(user);
     } catch (error) {
@@ -52,22 +69,5 @@ Router.delete("/delete/:userId", async (req, res) => {
         console.log(error);
     };
 });
-
-// Route pour actualizer un Utilisateur existant
-Router.put('/update/:userId', async (req, res) => {
-    try {
-        const user = await User.updateOne({
-            _id: req.params.userId
-        },{
-            name: req.body.userName,
-            age: req.body.userAge,
-            role: req.body.userRole
-        }); // (filter, data updated)
-        res.send(user);
-        console.log(user);
-    } catch (error) {
-        console.log(error);
-    };
-})
 
 module.exports = Router;
